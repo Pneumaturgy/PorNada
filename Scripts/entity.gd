1faf484cff27f3ef89aa43ecfbde55653f128487
Dylan Growcoot <93146289+Ghigog@users.noreply.github.com>
2024-07-20 15:44:05 -0500

    Added full vector input
    Also a lil arrow on the vector folder

--- Scripts/entity.gd
+++ Scripts/entity.gd
@@ -1,16 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 300.0
+#const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * speed
 
 
 func _physics_process(delta):
+	
+	get_input()
+	move_and_slide()
 	# Add the gravity.
 	#if not is_on_floor():
 		#velocity.y += gravity * delta
@@ -21,10 +26,12 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#var direction = Vector2(Input.get_axis("move_left", "move_right"),Input.get_axis("move_up","move_down"))
+	#if direction:
+		#velocity = direction * speed
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, speed)
+		#velocity.y = move_toward(velocity.y, 0, speed)
+		#velocity = move_toward()
 
-	move_and_slide()
+	
