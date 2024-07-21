46bd3a67fc2a352f6f7fd03461a1719952c57af8
Dylan Growcoot <93146289+Ghigog@users.noreply.github.com>
2024-07-20 19:06:36 -0500

    player can now fire bullets and enemies die

--- Scripts/TestDamagePayload.gd
+++ Scripts/TestDamagePayload.gd
@@ -1,20 +1,15 @@
 extends Payload
-class_name TestDamagePayload
+class_name DamagePayload
 
-@export var horizontal_speed = 0
-@export var vertical_speed = -1
+@export var speed = 500
+var direction
 
 func _init():
 	affected_properties_with_deltas = {
 		"health": -10.0
 	}
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.y += (vertical_speed * delta)
-	position.x += (horizontal_speed * delta)
+	position += direction * delta * speed
--- Scripts/payload.gd
+++ Scripts/payload.gd
@@ -1,7 +1,15 @@
 extends Area2D
 class_name Payload
 
+@export var timeToLive = 1.0
 var affected_properties_with_deltas = {}
+
+func _ready():
+	var death_timer = Timer.new()
+	add_child(death_timer)
+	death_timer.wait_time = timeToLive
+	death_timer.timeout.connect(destroy_bullet)
+	death_timer.start()
 
 func apply_effects(entity):
 	print("applying effects...")
@@ -14,3 +22,7 @@ func _on_body_entered(body):
 	print("body: ", body)
 	if body is CharacterBody2D: # Entity:
 		apply_effects(body)
+		destroy_bullet()
+
+func destroy_bullet():
+	queue_free()
