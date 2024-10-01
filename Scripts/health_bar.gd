de6de692b12efaa8ffb7289bed4eae46a25f727b
@Raphael-Marchetti-Calciolari <rapha.marchetti@gmail.com>
2024-10-01 20:39:53 -0300

    Adjusted the Health bar

--- Scripts/health_bar.gd
+++ Scripts/health_bar.gd
@@ -1,13 +1,25 @@
 extends ProgressBar
 
 
-@export var entity : Entity
-
+@onready var progress_bar = $"."
+@onready var entity : Entity
+@onready var offset
 
 func _ready():
+	entity = get_parent()
 	entity.healthChanged.connect(update)
+	 # TODO: adjust if enemy recovers health with time and the initial is not the max
+	max_value = entity.get_property("health")
+	offset =  global_position - entity.global_position
 	update()
-	
+
+
+func _process(delta):
+	global_position = entity.global_position + offset
+	var negative_rotation = (-1) * entity.global_rotation
+	rotation = negative_rotation
+
+
 func update():
 	print("Updating health bar")
 	value = entity.get_property("health")
