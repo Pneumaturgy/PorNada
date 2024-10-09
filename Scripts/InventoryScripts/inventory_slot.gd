0214d096f3a7b00cba1968fb2acd4f73a47b97f1
@Raphael-Marchetti-Calciolari <rapha.marchetti@gmail.com>
2024-10-08 22:22:21 -0300

    Added godot drag and drop managed functions

--- Scripts/InventoryScripts/inventory_slot.gd
+++ Scripts/InventoryScripts/inventory_slot.gd
@@ -13,3 +13,18 @@ func update_slot():
 	texture = current_item.item_image
 	quantity_text.text = "[center]" + str(quantity) + "[/center]"
 	
+func _get_drag_data(at_position):
+	# Retrieves information about the thing we are dragging
+	print("Dragging")
+	var data = SlotData.new(current_item, quantity)
+	return data
+	
+func _can_drop_data(at_position, data):
+	# Check if we can drop data on this slot
+	print("Can drop")
+	return true
+
+func _drop_data(at_position, data):
+	# What happens when we drop data on this resource
+	print("Dropped data: ", data)
+	pass
